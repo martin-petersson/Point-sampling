6c55ccd2c5304898450cf4b7a2c9f4229aef728b
martin-petersson <martin-petersson-art@proton.me>
2025-06-07 02:40:52 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+Blender script for demonstrating uniform point sampling on polygonal meshes.
+
+## Usage
+
+Open or paste script in the text editor, run or press
+
+```text
+alt + p
+```
+
+Hold shift in the 3d viewport and select instance object, then select the mesh to populate with instances.
+
+## Limitations
+
+* Only works on triangulated meshes at the moment.
---
+++ pointsampling.py
@@ -0,0 +1,103 @@
+import bpy
+import math
+import random
+
+population = 200
+
+def random_point(polypicklist):
+
+	randompoly = []
+
+	polychoice = random.choice(polypicklist)
+	randompoly.append(polychoice)
+
+	A = randompoly[0][0]
+	B = randompoly[0][1]
+	C = randompoly[0][2]
+
+	u = random.uniform(0.0, 1.0)
+	v = random.uniform(0.0, 1.0)
+
+	if (u + v) <= 1:
+		
+		random_point.result = ((((B - A) * u) + ((C - A) * v)) + A)
+			
+	# if point outside
+	elif (u + v) > 1:
+		
+		# flip the direction
+		u = 1 - u
+		v = 1 - v
+		
+		random_point.result = ((((B - A) * u) + ((C - A) * v)) + A)
+
+	return
+
+context = bpy.context
+
+selection = context.selected_objects
+
+active_canvas = selection[1]
+active_instance = selection[0]
+
+
+# get all polygons, their areas, vertices
+
+polylist = [] #get all polys and put them in a list
+arealist = [] # list to store area of polys
+
+for poly in active_canvas.data.polygons:
+	
+	polyarea = poly.area # get area of polygon
+	arealist.append(polyarea)
+	vertlist = []
+
+	for loop_index in poly.vertices:  # iterate through all vertices on every face index
+
+		# iterate through all vertices, multiply it to world space
+		k = (active_canvas.matrix_world @ active_canvas.data.vertices[loop_index].co)  # append vertices to list
+		vertlist.append(k)
+		
+	polylist.append(vertlist)
+
+# cumulative distribution:
+
+# sort arealist
+sortedareas = arealist[:]
+sortedareas.sort()
+smallest_area = sortedareas[0]
+cumul_distribution = []
+
+# step through all area values in arealist and divide with the smallest area
+for o in range(len(arealist)):
+	
+	divide_by_smallest= math.ceil(arealist[o] / smallest_area)
+	cumul_distribution.append(divide_by_smallest)
+	
+polypicklist = []
+
+for u in range(len(cumul_distribution)):
+	
+	list_step = cumul_distribution[u]
+	
+	for g in range(list_step):
+		
+		distrib_polylist = polylist[u]
+		polypicklist.append(distrib_polylist)
+		
+	pointlist = []
+
+for t in range(population):
+	
+	random_point(polypicklist)
+	pointlist.append(random_point.result)
+
+for l in pointlist:
+	
+	duplicate = active_instance.copy()
+	duplicate.data = active_instance.data.copy()
+	duplicate.name = active_instance.name + "_instance_"
+	
+	context.collection.objects.link(duplicate)
+	
+	duplicate.location = l
